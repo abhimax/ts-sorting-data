50edfdcb28a1bd6bf296bc654afbc9255d2af9be
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-03-02 04:51:13 +0530

    feat: add sort data

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-console.log("Hello TS!");
-const logData = () => {
-  console.log("log data!");
-};
-logData();
+class SortData {
+  constructor(public data: number[]) {}
+  sort(): void {
+    throw new Error("Method not implemented.");
+  }
+}
+
+const dateArray = [
+  new Date(2010, 9, 24),
+  new Date(2035, 9, 24),
+  new Date(2001, 9, 24),
+  new Date(2029, 9, 24),
+];
+
+const years = dateArray.map((date) => {
+  return date.getFullYear();
+});
+const sortData = new SortData(years);
+sortData.sort();
+console.log(years);
