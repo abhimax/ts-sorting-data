ce2f47e812104a9f7fd0040dee6a0b401bb8b827
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-03-05 10:09:14 +0530

    fix: destructure the length getter

--- src/SortData.ts
+++ src/SortData.ts
@@ -3,7 +3,7 @@ import { Numbers } from "./Numbers";
 export class SortData {
   constructor(public data: Numbers) {}
   sort(): void {
-    const length = this.data.length;
+    const { length } = this.data;
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
         if (this.data.compare(j, j + 1)) {
